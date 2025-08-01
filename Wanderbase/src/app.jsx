7ba7a59a760005abebd2c71a3a105930db9b2fc4
Lugi-alo <ejolohan@gmail.com>
2025-08-01 12:04:45 +0100

    feat(home): add routing and relocated homepage layout

--- Wanderbase/src/app.jsx
+++ Wanderbase/src/app.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import NavBar from "./components/nav-bar/navBar.jsx";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import Home from "./pages/home/home.jsx";
-import Perks from "./components/perk-section/perks.jsx";
 
 function App() {
-    console.log("App loaded");
     return (
-    <div>
-        <NavBar/>
-        <Home />
-        <Perks />
-    </div>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>} />
+            </Routes>
+        </BrowserRouter>
     );
 }
 
--- Wanderbase/src/pages/home/home.jsx
+++ Wanderbase/src/pages/home/home.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import Perks from "../../components/home/perk-section/perks.jsx";
+import NavBar from "../../components/common/nav-bar/navBar.jsx";
 
 export default function Home() {
-    return <h1> Home </h1>;
+    return (
+        <div>
+            <NavBar />
+            <Perks />
+        </div>
+    )
 }
