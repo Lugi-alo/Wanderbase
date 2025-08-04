6e20b271a95c9e96a5c85949fba3ba356cd7227e
Lugi-alo <ejolohan@gmail.com>
2025-08-04 17:24:06 +0100

    feat(navbar): search overlay jsx added

--- Wanderbase/src/components/common/nav-bar/navBar.jsx
+++ Wanderbase/src/components/common/nav-bar/navBar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import {useState, useEffect} from "react";
 
 export default function NavBar() {
+    const [open, setOpen] = useState(false);
+    
     return (
         <nav className="nav">
-            <a href="/" className="home"> Home </a>
+            <a href="/"> Home </a>
             <ul>
-                <li> 
-                    <a href="/sign-up"> Sign Up </a>
-                </li>
+                <li><a href="/Sign-up"> Sign Up </a> </li>
+                <li><a href="/Login"> Login </a></li>
             </ul>
+
+            <button onClick={() => setOpen(!open)}> Search </button>
+
+            {open && (
+                <div className="search-input">
+                    <input type="text" placeholder="Search hotels or reviews..." autoFocus />
+                </div>
+            )}
+        
         </nav>
     );
 }
