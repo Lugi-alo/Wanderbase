355a6644df51d816d41615f381e82c9b9baff581
Lugi-alo <ejolohan@gmail.com>
2025-08-05 22:28:33 +0100

    styles(navbar): added initial navbar styles

--- Wanderbase/src/components/common/nav-bar/navBar.jsx
+++ Wanderbase/src/components/common/nav-bar/navBar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import {useState, useEffect} from "react";
 
+import "./navBar.scss";
+
 export default function NavBar() {
     const [open, setOpen] = useState(false);
+
+    let searchClass = 'search-input';
+    if (open) {
+        searchClass += ' visible';
+    }
     
     return (
         <nav className="nav">
             <a href="/"> Home </a>
+            
             <ul>
                 <li><a href="/Sign-up"> Sign Up </a> </li>
                 <li><a href="/Login"> Login </a></li>
             </ul>
 
-            <button onClick={() => setOpen(!open)}> Search </button>
+            <div className="nav-actions">
+                <button onClick={() => setOpen(!open)}> Search </button>
+            </div>
 
-            {open && (
-                <div className="search-input">
-                    <input type="text" placeholder="Search hotels or reviews..." autoFocus />
-                </div>
-            )}
+            <div className={searchClass}>
+                <input type="text" placeholder="Search hotels or reviews..." autoFocus />
+            </div>
         
         </nav>
     );
