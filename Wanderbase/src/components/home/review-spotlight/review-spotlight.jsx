1874b7c0fbdac94b7f3f836926dff6cb48b284b6
Lugi-alo <ejolohan@gmail.com>
2025-08-09 16:02:46 +0100

    styles(review spotlight): added review container styles

--- Wanderbase/src/components/home/review-spotlight/review-spotlight.jsx
+++ Wanderbase/src/components/home/review-spotlight/review-spotlight.jsx
@@ -1,38 +1,52 @@
 import React from 'react';
 
+import './review-spotlight.scss';
+import UserIcon from '../../../assets/icons/userIcon.png';
+import StockHotel from '../../../assets/images/hotel.jpg';
+
 export default function ReviewSpotlight() {
     
      const FeaturedReview = () => {
         const featuredReview = [
         {
             id: 1,
-            title: "Featured Review",
-            image: "placeholder review image",
-            rating: "review rating",
-            content: "review content",
-            user: "User X",
-            userAvatar: "placeholer avatar",
+            title: "Brilliant Stay at Hotel X",
+            image: StockHotel,
+            rating: "⭐⭐⭐⭐⭐",
+            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ull",
+            user: "John Doe",
+            userAvatar: UserIcon,
         },
     ]
 
     return (
-        <section className="review-spotlight">
-            {featuredReview.map((review, index) => (
-            <div className="review" key={index}>
-                <div className="review-header">
-                    <h3>{review.title}</h3>
-                    <p>{review.user}</p>
-                    <p> {review.userAvatar}</p>
-                </div>
-                <div className="review-content">
-                    <p> {review.image} </p>
-                    <p>Rating: {review.rating}</p>
-                    <p>{review.content}</p>
-                    <p>Written by: {review.user}</p>
-                </div>
+  <section className="review-spotlight">
+    {featuredReview.map((review, index) => (
+      <div className="review" key={index}>
+        <div className="review-content">
+          <div className="review-image">
+            <img src={review.image}/>
+          </div>
+
+          <div className="review-details">
+            <div className="review-header">
+              <h3>{review.title}</h3>
             </div>
-            ))}
-        </section>
+
+            <div className="review-text">
+              <p className="review-rating">{review.rating}</p>
+              <p>{review.content}</p>
+            </div>
+
+            <div className="review-footer">
+              <img src={review.userAvatar} />
+              <p>{review.user}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    ))}
+  </section>
 );
 }
     return <FeaturedReview />
